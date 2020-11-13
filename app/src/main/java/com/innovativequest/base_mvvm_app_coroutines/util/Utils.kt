6aca11c8629a5c8e5650cc3d58cd408e918a5dc7
Ghous Khan <ghousrk@gmail.com>
2020-11-13 17:41:04 +0000

    Trial changes to fix circleci error

--- app/src/main/java/com/innovativequest/base_mvvm_app_coroutines/util/Utils.kt
+++ app/src/main/java/com/innovativequest/base_mvvm_app_coroutines/util/Utils.kt
@@ -8,14 +8,6 @@ import java.util.*
  */
 object Utils {
 
-    private val INPUT_DATE_FORMAT_FULL = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss")
-    private val OUTPUT_DATE_FORMAT = SimpleDateFormat("dd MMMM yyyy")
-
-
-    fun getDateInDisplayableFormat(inputDateStr: String?): String{
-        return OUTPUT_DATE_FORMAT.format(INPUT_DATE_FORMAT_FULL.parse(inputDateStr))
-    }
-
     fun getCurrentTime(): Long {
         return Date().time
     }
